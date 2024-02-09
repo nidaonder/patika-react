6112e0016ca3c1a7e7e8b9ab5fe23a01d9c90d01
nidaonder <nidaaonder@gmail.com>
2024-02-09 23:47:41 +0300

    added context notes.

---
+++ week-22/context/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Button from "./components/Button";
+import ThemeContext from "./context/ThemeContext";
+
+function App() {
+  return <ThemeContext.Provider value="dark">
+    <Button />
+  </ThemeContext.Provider>;
+}
+
+export default App;
